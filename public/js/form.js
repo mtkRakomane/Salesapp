55de26487a29ea219b886d7e9fed1345dc539b4c
mtkRakomane <thabangkennedy36@gmail.com>
2025-08-08 14:56:48 +0200

    UPDATED THE scarface  DROPDOWNS

--- public/js/form.js
+++ public/js/form.js
@@ -4,17 +4,24 @@ const saleEmailInput = document.getElementById('email');
 const saleRoleInput = document.getElementById('role');
 salePersonSelect.addEventListener('change', function () {
   const selectedOption = salePersonSelect.options[salePersonSelect.selectedIndex];
-  saleCellInput.value = selectedOption.dataset.cell;
-  saleEmailInput.value = selectedOption.dataset.email;
-  saleRoleInput.value = selectedOption.dataset.role;
+  saleCellInput.value = selectedOption.dataset.cell || '';
+  saleEmailInput.value = selectedOption.dataset.email || '';
+  saleRoleInput.value = selectedOption.dataset.role || '';
 });
 const ebmCameraSelect = document.getElementById('cameras');
 const ratePerCameraInput = document.getElementById('rate_per_camera');
 const linkFeeInput = document.getElementById('linkup_fee');
 ebmCameraSelect.addEventListener('change', function () {
   const selectedOption = ebmCameraSelect.options[ebmCameraSelect.selectedIndex];
-  const rate = selectedOption.dataset.rate_per_camera || 0;
-  const linkFee = selectedOption.dataset.linkup_fee || 0;
-  ratePerCameraInput.value = `R ${parseFloat(rate).toFixed(2)}`;
-  linkFeeInput.value = `R ${parseFloat(linkFee).toFixed(2)}`;
+  const rate = parseFloat(selectedOption.dataset.rate_per_camera) || 0;
+  const linkFee = parseFloat(selectedOption.dataset.linkup_fee) || 0;
+  ratePerCameraInput.value = `R ${rate.toFixed(2)}`;
+  linkFeeInput.value = `R ${linkFee.toFixed(2)}`;
+});
+const scarfaceRateSelect = document.getElementById('noScarfaceCamera');
+const monthlyRateInput = document.getElementById('monthlyRate');
+scarfaceRateSelect.addEventListener('change', function() {
+  const selectedOption = scarfaceRateSelect.options[scarfaceRateSelect.selectedIndex];
+  const monthrate = parseFloat(selectedOption.dataset.monthlyRate) || 0;
+  monthlyRateInput.value = `R ${monthrate.toFixed(2)}`;
 });
